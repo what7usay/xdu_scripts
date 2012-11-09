507c329b7aa2b47c7aebf177c60754d0734fb918
hoox <matwet.u@gmail.com>
2012-11-09 16:32:10 +0800

    xdjwc login helper

---
+++ chromiumextension/LoginHelper/content.js
@@ -0,0 +1,57 @@
+try{
+	form = document.getElementsByName("form1")[0];
+	uid = document.getElementsByName("uid")[0];
+	psw = document.getElementsByName("password")[0];
+
+	//Let's fill the form...
+	uid.value = "01093033";
+	psw.value = "19900724";
+
+	//A dirty hack to call the webpage function;
+	body = document.getElementsByTagName("body")[0];
+	body.innerHTML += '<input id="whtthefck" type="button" onclick="return window.change();" display-style:none>';
+	document.getElementById("whtthefck").onclick();
+
+	//now we have the encrypted value
+	uidval = uid.value;
+	pswval = psw.value;
+	formdata = "uid=" + uidval + "&password=" + pswval + "&command=studentLogin" ;
+
+	//post to the server
+	var xhr = new  XMLHttpRequest();
+	xhr.open("POST",form.action);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.onload = function() {
+		//where do you want to go after login
+		dest=5;
+		//1.学习主页
+		//2.理论课成绩
+		//3.公共任选课
+		//4.网上选课
+		//5.毕设选题
+//		var url="http://125.76.215.232/xdjwWeb/";
+		var url="../";
+		switch(dest){
+			case 1:
+				url+="main_study.jsp";
+				break;
+			case 2:
+				url+="studentStatus/queryScore/query_person_score.jsp";
+				break;
+			case 3:
+				url+="studentStatus/arbAmin/select_arb_course_top.jsp";
+				break;
+			case 4:
+				url+="Servlet/CourseScoreControl?command=test_st_indexShowTeachPlan";
+				break;
+			case 5:
+				url+="practiceAdmin/graduationDesign/studentChooseTitle/show_choose_title.jsp";
+				break;
+		}
+		window.location.href=url;
+	}
+	xhr.send(formdata);
+
+}catch(e){
+	alert(e);
+}
